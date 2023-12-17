099b8966903eaa4f2fd9623c91541da6c6d628ba
熊天成 <728611827@qq.com>
2023-12-17 15:38:45 +0800

    111

---
+++ src/views/About.vue
@@ -0,0 +1,29 @@
+
+ <template>
+  <div class="about">
+    <s-header :name="'关于我们'"></s-header>
+    <div class="about-body">
+      八组 网易商城
+    </div>
+  </div>
+</template>
+
+<script setup>
+import sHeader from '@/components/SimpleHeader.vue'
+</script>
+
+<style lang="less" scoped>
+  .about {
+    box-sizing: border-box;
+    padding: 20px;
+    .intro-img {
+      width: 100%;
+    }
+    .about-body {
+      font-size: 16px;
+      a {
+        color: #007fff;
+      }
+    }
+  }
+</style>
